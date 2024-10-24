19414a517884ceb3313e0238833a2c97eccc1d54
Daniel Mesu <danielrojo1927@gmail.com>
2024-10-24 17:04:22 +0100

    adding image support

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { EyeIcon, HeartIcon } from "@heroicons/react/24/outline";
 import { LoaderFunction } from "@remix-run/node";
 import { json, Link, useLoaderData } from "@remix-run/react";
 import Navbar from "~/components/Navbar";
@@ -26,24 +27,32 @@ export default function Index() {
               {data.map((product: any, index) => (
                 <div
                   key={product._id}
-                  className='bg-white rounded-lg shadow-lg p-6'
+                  className='bg-white rounded-lg shadow-lg p-0'
                 >
                   <img
-                    className='h-48 w-full object-cover rounded-t-lg'
-                    src={""}
+                    className='h-36 w-full object-cover rounded-t-lg'
+                    src={product.imageUrl}
                     alt={product.name}
                   />
-                  <div className='mt-4'>
+                  <div className='p-5 mt-4'>
                     <h2 className='text-lg font-semibold text-gray-900'>
                       {product.name}
                     </h2>
-                    <p className='text-gray-600'>{product.price}</p>
-                    <Link
-                      to={`/products/${product._id}`}
-                      className='mt-2 inline-block bg-blue-500 text-white px-4 py-2 rounded-lg'
-                    >
-                      View Product
-                    </Link>
+                    <p className='text-gray-600'>$ {product.price}</p>
+                    <div className='mt-2 inline-flex items-center'>
+                      <Link
+                        to={`/products/${product._id}`}
+                        className='bg-blue-500 text-white p-2 rounded-lg mr-2' // Added margin to separate icons
+                      >
+                        <EyeIcon className='h-5 w-5' aria-hidden='true' />
+                      </Link>
+                      <Link
+                        to={`/favorites/${product._id}`} // Update this route as needed
+                        className='bg-red-500 text-white p-2 rounded-lg' // Change color for differentiation
+                      >
+                        <HeartIcon className='h-5 w-5' aria-hidden='true' />
+                      </Link>
+                    </div>
                   </div>
                 </div>
               ))}
